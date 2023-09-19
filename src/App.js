786520ce2302a7660b622dc59d9c12bc01380cd2
samsher2578 <samsherkhan2578@gmail.com>
2023-09-19 13:13:53 +0530

    UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <div className="center">
+          <br />
+          <h1>ToDo List</h1>
+          <br />
+          <input type="text" placeholder='Add a Item'/>
+          <button> + </button>
+
+          <ol>
+            <li>apple</li>
+          </ol>
+        </div>
+      </div>
     </div>
   );
 }
