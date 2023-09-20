279ae8f4d49e95d503ea2e8ebc898938fa922b88
samsher2578 <samsherkhan2578@gmail.com>
2023-09-20 07:29:09 +0530

    Add List Element - HOOK

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 
+import { useState } from 'react';
 import './App.css';
+import ToDoList from './ToDoList';
 
 function App() {
+
+  const [inputList, setInputList] = useState("");
+  const [item, setItem] = useState([]);
+
+  const itemEvent = (event) => {
+    setInputList(event.target.value);
+  }; 
+
+  const itemList = () => {
+    setItem((oldItem) => {
+      return [...oldItem, inputList];
+    })
+
+    setInputList("");
+  };
+
   return (
     <div className="App">
       <div className="main">
@@ -9,11 +27,14 @@ function App() {
           <br />
           <h1>ToDo List</h1>
           <br />
-          <input type="text" placeholder='Add a Item'/>
-          <button> + </button>
+          <input type="text" placeholder='Add a Item' value={inputList} onChange={itemEvent}/>
+          <button onClick={itemList}> + </button>
 
           <ol>
-            <li>apple</li>
+
+            {item.map((itemval) => {
+              return <ToDoList text = {itemval} />;
+            })}
           </ol>
         </div>
       </div>
